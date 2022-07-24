5f67912cb54f65ecd2f32921058c2367421f5d6c
Emily Stroud <emilystroud93@gmail.com>
2022-07-24 17:54:34 -0500

    added all file structure

---
+++ pages/team/[firebaseKey].js
@@ -0,0 +1,17 @@
+// import React, { useEffect, useState } from 'react';
+// import { useRouter } from 'next/router';
+
+// export default function viewTeam() {
+//   const [teamDetails, setTeamDetails] = useState({});
+//   const router = useRouter();
+
+//   const { firebaseKey } = router.query;
+
+//   useEffect(() => {
+
+//   }, );
+
+//   return (
+//     // TEAM CARD HERE
+//   );
+// }
