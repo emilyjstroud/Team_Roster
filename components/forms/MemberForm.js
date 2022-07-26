24628b570dbd1999671b159bfaef80d513a57d46
Emily Stroud <emilystroud93@gmail.com>
2022-07-26 18:52:03 -0500

    working out form errors

--- components/forms/MemberForm.js
+++ components/forms/MemberForm.js
@@ -4,23 +4,24 @@ import { Form } from 'react-bootstrap/Form';
 import { FloatingLabel } from 'react-bootstrap/FloatingLabel';
 import { Button } from 'react-bootstrap/Button';
 import PropTypes from 'prop-types';
-import { createMember, getMembers, updateMember } from '../../api/memberData';
+import { createMember, updateMember } from '../../api/memberData';
 import { useAuth } from '../../utils/context/authContext';
 
 const initialState = {
   name: '',
   role: '',
   image: '',
+  firebaseKey: '',
 };
 
 function MemberForm({ obj }) {
   const [formInput, setFormInput] = useState(initialState);
-  const [teams, setMembers] = useState([]);
+  // const [teams, setMembers] = useState([]);
   const router = useRouter();
   const { user } = useAuth();
 
   useEffect(() => {
-    getMembers(user.uid).then(setMembers);
+    // getMembers(user.uid).then(setMembers);
 
     if (obj.firebaseKey) setFormInput(obj);
   }, [obj, user]);
@@ -37,7 +38,7 @@ function MemberForm({ obj }) {
     e.preventDefault();
     if (obj.firebaseKey) {
       updateMember(formInput)
-        .then(() => router.push(`/members/${obj.firebaseKey}`));
+        .then(() => router.push('/members'));
     } else {
       const payload = { ...formInput, uid: user.uid };
       createMember(payload).then(() => {
@@ -50,7 +51,7 @@ function MemberForm({ obj }) {
     <Form onSubmit={handleSubmit}>
       <h2 className="text-white mt-5">{obj.firebaseKey ? 'Update' : 'Create'} Member</h2>
       <FloatingLabel controlId="floatingInput1" label="Member Name" className="mb-3">
-        <Form.Control type="text" placeholder="Member's Name" name="member" value={formInput.name} onChange={handleChange} required />
+        <Form.Control type="text" placeholder="Member's Name" name="name" value={formInput.name} onChange={handleChange} required />
       </FloatingLabel>
       <FloatingLabel controlId="floatingInput2" label="Member Image" className="mb-3">
         <Form.Control type="url" placeholder="Enter an image url" name="image" value={formInput.image} onChange={handleChange} required />
@@ -59,15 +60,15 @@ function MemberForm({ obj }) {
         <Form.Control type="text" placeholder="Role" name="role" value={formInput.role} onChange={handleChange} required />
       </FloatingLabel>
 
-      <FloatingLabel controlId="floatingSelect" label="Team">
+      {/* <FloatingLabel controlId="floatingSelect" label="Team">
         <Form.Select
           aria-label="Team"
           name="teams"
           onChange={handleChange}
           className="mb-3"
           required
-        >
-          <option value="">Select a Team</option>
+        > */}
+      {/* <option value="">Select a Team</option>
           {
           teams.map((team) => (
             <option
@@ -76,17 +77,17 @@ function MemberForm({ obj }) {
               selected={obj.name === team.firebaseKey}
             >
               {team.name}
-            </option>
-          ))
-        }
-        </Form.Select>
-      </FloatingLabel>
+            </option> */}
+      {/* ))
+          } */}
+      {/* </Form.Select> */}
+      {/* </FloatingLabel> */}
 
-      <FloatingLabel controlId="floatingTextarea" label="Description" className="mb-3">
-        <Form.Control as="textarea" placeholder="Description" style={{ height: '100px' }} name="description" value={formInput.description} onChange={handleChange} required />
-      </FloatingLabel>
+      {/* // <FloatingLabel controlId="floatingTextarea" label="Description" className="mb-3">
+        //   <Form.Control as="textarea" placeholder="Description" style={{ height: '100px' }} name="description" value={formInput.description} onChange={handleChange} required />
+        // </FloatingLabel> */}
 
-      {/* A WAY TO HANDLE UPDATES FOR TOGGLES, RADIOS, ETC  */}
+      {/* A WAY TO HANDLE UPDATES FOR TOGGLES, RADIOS, ETC
       <Form.Check
         className="text-white mb-3"
         type="switch"
@@ -98,7 +99,7 @@ function MemberForm({ obj }) {
           ...prevState,
           sale: e.target.checked,
         }))}
-      />
+      /> */}
       <Button type="submit">{obj.firebaseKey ? 'Update' : 'Create'} Member</Button>
     </Form>
   );
--- pages/team.js
+++ pages/team.js
@@ -1,22 +1,45 @@
-// import React, { useEffect, useState } from 'react';
-// import { useAuth } from '../utils/context/authContext';
+import React, { useEffect, useState } from 'react';
+// import { Card } from 'react-bootstrap/Card';
+// import { deleteSingleTeam, getTeam } from '../api/teamData';
+import { getTeam } from '../api/teamData';
+import { useAuth } from '../utils/context/authContext';
+import TeamCard from '../components/TeamCard';
 
-// function TeamPage() {
-// const [team, setTeam] = useState([]);
+function TeamPage() {
+  const [teams, setTeam] = useState([]);
 
-// const { user } = useAuth();
+  const { user } = useAuth();
 
-// const getWholeTeam = () => {
+  const getWholeTeam = () => {
+    getTeam(user.uid).then(setTeam);
+  };
 
-// };
+  useEffect(() => {
+    getWholeTeam();
+  }, [user]);
 
-// useEffect(() => {
-//   getWholeTeam();
-// }, []);
+  return (
+    // <Card style={{ width: '18rem', margin: '10px' }}>
+    //   <Card.Body>
+    //     <Card.Title>{teamObj.name}</Card.Title>
+    //     <Link href={`/members/${teamObj.firebaseKey}`} passHref>
+    //       <Button variant="primary" className="m-2">VIEW</Button>
+    //     </Link>
+    //     <Link href={`/members/edit/${teamObj.firebaseKey}`} passHref>
+    //       <Button variant="info">EDIT</Button>
+    //     </Link>
+    //     <Button variant="danger" onClick={deleteSingleTeam} className="m-2">
+    //       DELETE
+    //     </Button>
+    //   </Card.Body>
+    // </Card>
+    <div className="d-flex flex-wrap">
+      {/* TODO: map over books here using BookCard component */}
+      {
+    teams.map((team) => <TeamCard key={team.firebaseKey} teamObj={team} onUpdate={getWholeTeam} />)
+    }
+    </div>
+  );
+}
 
-//   return (
-//     // TEAM CARD HERE
-//   );
-// };
-
-// export default TeamPage;
+export default TeamPage;
